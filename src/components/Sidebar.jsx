19a418806be1cec4d4acaf82f40178151c31084e
Jeremy <marszalekbcn@gmail.com>
2023-08-04 00:55:34 +0200

    publish

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -32,18 +32,18 @@ const SidebarClosed = () => {
       ]
 return(
 
-<div className= {` ${isVisible ? 'w-[250px]' : 'w-[59px]'} h-screen bg-white justify-start items-start flex`}>
-    <div className={` ${isVisible ? 'w-[250px]' : 'w-[59px]'} self-stretch pt-4 flex-col justify-start items-start gap-4 inline-flex`}>
+<div className= {` ${isVisible ? 'w-[250px] duration-300' : 'w-[59px]'} h-screen bg-white justify-start items-start flex`}>
+    <div className={` ${isVisible ? 'w-[250px] duration-300' : 'w-[59px]'} self-stretch pt-4 flex-col justify-start items-start gap-4 inline-flex`}>
     <div className=" pl-5 pt-5 justify-start items-start gap-2.5 inline-flex">
             <img className="min-w-6 min-h-6 relative" alt="logo" src={Logo} />
             {isVisible &&
-            <img className="text-gray-900 text-2xl font-bold leading-9" alt="logo" src={Sapiologie}/>
+            <img className="text-gray-900 text-2xl font-bold leading-9 duration-300" alt="logo" src={Sapiologie}/>
             }
         </div>
         <div className="w-[59px] h-screen grow shrink basis-0 flex-col justify-between items-start gap-4 flex">
             <div className="self-stretch h-auto flex-col justify-start items-start gap-4 flex">
                 {isVisible?  
-                <ul className="h-auto pt-4 flex-col justify-start items-start gap-4 flex">
+                <ul className="h-auto pt-4 flex-col justify-start items-start gap-4 flex duration-300">
                 {Menu1.map((Menu) => (
                      <HexagonOpen Menu={Menu}/>
                 ))}                    
@@ -59,7 +59,7 @@ return(
                 }
                 <div className={` ${isVisible ? 'w-[250px]' : 'w-[59px]'} h-px bg-gray-200`} />
                 {isVisible?  
-                <ul className="h-auto pt-4 flex-col justify-start items-start gap-4 flex">
+                <ul className="h-auto pt-4 flex-col justify-start items-start gap-4 flex duration-300">
                 {Menu2.map((Menu, index) => (
                 <HexagonOpen Menu={Menu}/>
                  ))}                    
@@ -75,21 +75,21 @@ return(
             </div>
             <div className="self-stretch h-[116px] pt-3 pb-4 flex-col justify-end items-start gap-2 flex">
             <div className="flex">                    
-            <button className={` ${isVisible ? 'w-[250px] pl-5 gap-2.5 justify-start' : 'w-[59px] justify-center py-2'} h-10 items-center gap-[17px] inline-flex`} type="button" onClick={() => handleClick()}>
+            <button className={` ${isVisible ? 'w-[250px] pl-5 gap-2.5 justify-start duration-300' : 'w-[59px] justify-center py-2'} h-10 items-center gap-[17px] inline-flex`} type="button" onClick={() => handleClick()}>
                         {
-                            isVisible ? <img className="min-w-6 min-h-6 relative text-gray-600 " alt="chevron gauche" src={ChevronGauche } />
+                            isVisible ? <img className="min-w-6 min-h-6 relative text-gray-600 duration-300" alt="chevron gauche" src={ChevronGauche } />
                             :<img className="min-w-6 min-h-6 relative text-gray-500 " alt="chevron droite" src={ChevronDroite} />
 
                         }
                     {isVisible &&
-                    <p className=" text-gray-600 text-base font-normal leading-normal">Collapse</p>
+                    <p className=" text-gray-600 text-base font-normal leading-normal duration-300">Collapse</p>
                     }
                     </button>
                 </div>
                 <div className={` ${isVisible ? 'w-[250px] pl-5 gap-2.5 justify-start' : 'w-[59px] justify-center py-2'} h-10 items-center gap-[17px] inline-flex`}>
                     <img className="w-6 h-6 relative rounded-[60px]" alt="Portrait" src={Portrait}/>
                     {isVisible &&
-                    <div className=" text-gray-600 text-base font-normal leading-normal">John Doe</div>
+                    <div className=" text-gray-600 text-base font-normal leading-normal duration-300">John Doe</div>
                     }
                 </div>
             </div>
