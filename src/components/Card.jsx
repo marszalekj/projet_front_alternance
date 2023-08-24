c798ecd655704d06e4365483e08ee7554e46850a
Jeremy <marszalekbcn@gmail.com>
2023-08-24 19:15:36 +0200

    images

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,14 @@
-const Card = () => {
+import { Link } from "react-router-dom"
+
+const Card = (props) => {
     return (
 <div className="flex h-[134px] w-1/3  max-w-xs rounded-lg border border-gray-200 bg-gradient-to-tr from-teal-700 to-green-800 shadow">
+
+<Link to={props.site.link} key={props.site.id}>
+
+<img className="flex" src={props.site.cover} alt="cover image"/>
+
+</Link>
 
 </div>
 
