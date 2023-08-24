83d8fa9fff937978bf925a8f43b3fe3a07df6916
Jeremy <marszalekbcn@gmail.com>
2023-08-24 14:23:47 +0200

    json

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,19 @@
 import Card from "../components/Card";
 import Sidebar from "../components/Sidebar";
 
+import Sites from "../Sites.json"
+
 
 const Home = () => {
     return(
         <main className=" inline-flex h-screen w-screen items-start justify-start">
         <Sidebar/>
         <div className="flex h-screen w-screen flex-wrap content-start gap-4 px-8 py-4">
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+        {
+            Sites.map((site) => (
+                <Card key={site.id} site={site}/>
+            ))
+        }
         </div>
     </main>
     )
