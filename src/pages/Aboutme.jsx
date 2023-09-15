b0367a4b9de32808ef0d3a316c08eb07676eb8eb
Jeremy <marszalekbcn@gmail.com>
2023-09-15 15:56:04 +0200

    size txt aboutme

--- src/pages/Aboutme.jsx
+++ src/pages/Aboutme.jsx
@@ -23,12 +23,12 @@ const Aboutme = () => {
         <Glitch/>
         </div>
         <div className="flex flex-col pt-12">
-        <h2 className="animate-text bg-gradient-to-r from-black via-slate-500 to-black bg-clip-text  pl-28 pr-80 text-start text-xl font-medium text-transparent">
+        <h2 className="animate-text bg-gradient-to-r from-black via-slate-500 to-black bg-clip-text  pl-28 pr-80 text-start text-lg font-medium text-transparent">
         Hello world ! <br/> My name is Jérémy, a french guy living between Toulouse and Barcelona. After a <b>Master&apos;s degree in Digital Law</b> (Toulouse Capitole University) and <b>5 years in Data Privacy</b> field in Paris (as consultant in Financial Services and DPO in health sector), I completed a 9-month training course in 2023 to become a developer and am currently looking for <b>my first opportunity</b> as dev.
         </h2>
 
         <div className="flex h-48 w-screen items-center justify-center gap-x-8">
-        <h2 className="max-w-[310px] animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-black text-2xl text-black">Technologies I&apos;ve worked with </h2>
+        <h2 className="max-w-[310px] animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-black text-lg text-black">Technologies I&apos;ve worked with </h2>
         <img className="h-20 w-20" src={JSlogo}/>
         <img className="h-20 w-20" src={Reactlogo}/>
         <img className="h-20 w-20" src={Node}/>
@@ -36,12 +36,12 @@ const Aboutme = () => {
         <img className="h-20 w-20" src={Tailwind}/>
         </div>
         <div className="flex h-48 w-screen items-center justify-center gap-x-8">
-        <h2 className=" max-w-[300px] animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-black text-2xl text-black">Technologies I&apos;m discovering </h2>
+        <h2 className=" max-w-[300px] animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-black text-lg text-black">Technologies I&apos;m discovering </h2>
         <img className="h-20 w-20" src={Golang}/>
         <img className="h-20 w-20" src={Typescript}/>
         </div>
         <div className="flex h-48 w-screen items-center justify-center gap-x-8">
-        <h2 className="max-w-[520px] animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-black text-2xl text-black">Technologies I plan to master in the next few years </h2>
+        <h2 className="max-w-[520px] animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-black text-lg text-black">Technologies I plan to master in the next few years </h2>
         <img className="h-20 w-20" src={Solidity}/>
         <img className="h-20 w-20" src={Rust}/>
         </div>
